87c1cd367257738dd37201e503564cb80f07328b
Hyzyr <avezberdiyew@gmail.com>
2023-04-13 01:11:50 +0500

    contact page

--- js/slick-init.js
+++ js/slick-init.js
@@ -1,66 +1,13 @@
 const slickSliders = document.querySelectorAll(".slickSlider");
 if (slickSliders) {
   const slickSettings = {
-    companies: {
-      slidesToShow: 6,
-      infinite: true,
-      autoplay: true,
-      speed: 1500,
-      centerMode: true,
-      focusOnSelect: true,
-      arrows: false,
-      dots: false,
-      responsive: [
-        {
-          breakpoint: 1025,
-          settings: {
-            slidesToShow: 4,
-            speed: 1000,
-            autoplay: true,
-          },
-        },
-        {
-          breakpoint: 720,
-          settings: {
-            slidesToShow: 3,
-            speed: 1000,
-            autoplay: true,
-          },
-        },
-        {
-          breakpoint: 590,
-          settings: {
-            slidesToShow: 2.6,
-            speed: 1000,
-            autoplay: true,
-          },
-        },
-        {
-          breakpoint: 520,
-          settings: {
-            slidesToShow: 2,
-            speed: 1000,
-            autoplay: true,
-          },
-        },
-        {
-          breakpoint: 460,
-          settings: {
-            slidesToShow: 2,
-            cssEase: "linear",
-            speed: 2000,
-            autoplay: true,
-            autoplaySpeed: 0,
-          },
-        },
-      ],
-    },
     default: {
       slidesToShow: 3,
       speed: 600,
       arrows: true,
       dots: false,
       infinite: false,
+      focusOnSelect: true,
       prevArrow: $("#brandSlider .prevArrow"),
       nextArrow: $("#brandSlider .nextArrow"),
       responsive: [
@@ -68,63 +15,75 @@ if (slickSliders) {
           breakpoint: 1025,
           settings: {
             slidesToShow: 2.5,
-            speed: 1000,
-            autoplay: true,
           },
         },
         {
           breakpoint: 720,
           settings: {
             slidesToShow: 2,
-            speed: 1000,
-            autoplay: true,
           },
         },
         {
           breakpoint: 590,
           settings: {
             slidesToShow: 1.6,
-            speed: 1000,
-            autoplay: true,
           },
         },
         {
           breakpoint: 520,
           settings: {
             slidesToShow: 1.2,
-            speed: 1000,
-            autoplay: true,
           },
         },
         {
           breakpoint: 460,
           settings: {
             slidesToShow: 1,
-            speed: 1000,
-            autoplay: true,
           },
         },
       ],
     },
-    feedbacksImages: {
-      slidesToShow: 1,
-      speed: 300,
-      arrows: false,
+    reviews: {
+      slidesToShow: 3.25,
+      speed: 600,
+      arrows: true,
       dots: false,
-      fade: true,
-      cssEase: "ease-out",
-      asNavFor: "#feedbacksContent",
-    },
-
-    feedbacksContent: {
-      slidesToShow: 1,
-      speed: 0,
-      arrows: false,
-      dots: true,
-      appendDots: $("#feedbacksContentDots"),
-      fade: true,
-      cssEase: "linear",
-      asNavFor: "#feedbacksImages",
+      infinite: false,
+      focusOnSelect: true,
+      prevArrow: $("#brandSlider .prevArrow"),
+      nextArrow: $("#brandSlider .nextArrow"),
+      responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 2.5,
+          },
+        },
+        {
+          breakpoint: 720,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 590,
+          settings: {
+            slidesToShow: 1.6,
+          },
+        },
+        {
+          breakpoint: 520,
+          settings: {
+            slidesToShow: 1.2,
+          },
+        },
+        {
+          breakpoint: 460,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
     },
   };
   const getSlickSettings = (settings) => {
@@ -132,6 +91,14 @@ if (slickSliders) {
   };
   slickSliders.forEach((slider) => {
     const settings = slider.getAttribute("data-slick") ?? null;
-    $(slider).slick(getSlickSettings(settings));
+    const nextArrow = slider.parentElement.querySelector(".nextArrow") ?? null;
+    const prevArrow = slider.parentElement.querySelector(".prevArrow") ?? null;
+
+    $(slider).slick({
+      ...getSlickSettings(settings),
+      arrows: !!(nextArrow || prevArrow),
+      nextArrow,
+      prevArrow,
+    });
   });
 }
